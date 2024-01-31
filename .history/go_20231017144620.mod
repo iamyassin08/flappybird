96d6805f34ec33e645e5710e101d59f15b4433bb
iamyassin08 <yassinmohamed49@gmail.com>
2024-01-31 12:53:39 -0600

    flappy bird game

---
+++ .history/go_20231017144620.mod
@@ -0,0 +1,5 @@
+module flappygame
+
+go 1.20
+
+require github.com/gen2brain/raylib-go/raylib v0.0.0-20231017191853-10c61020bc6c
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"math/rand"
+	"strconv"
+	"time"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+type Apple struct {
+	posX  int32
+	posY  int32
+	image rl.Texture2D
+}
+
+func main() {
+	screenWidth := int32(800)
+	screenHeight := int32(450)
+	rl.InitAudioDevice()
+	eat_noise := rl.LoadSound("sound/eat.wav")
+	defer rl.UnloadSound(eat_noise)
+
+	rand.Seed(time.Now().UnixNano())
+	rl.InitWindow(screenWidth, screenHeight, "FlappyApples")
+	rl.SetTargetFPS(60)
+
+	bird_down := rl.LoadImage("assets/bird-down.png")
+	defer rl.UnloadImage(bird_down)
+	bird_up := rl.LoadImage("assets/bird-up.png")
+	defer rl.UnloadImage(bird_up)
+
+	texture := rl.LoadTextureFromImage(bird_up)
+	defer rl.UnloadTexture(texture)
+
+	appleImage := rl.LoadImage("assets/apple.png")
+	rl.ImageResize(appleImage, 25, 25) // Resize the apple image to a smaller size
+	defer rl.UnloadImage(appleImage)
+
+	var x_coords int32 = screenWidth/2 - texture.Width/2
+	var y_coords int32 = screenHeight/2 - texture.Height/2 - 40
+	var score int = 0
+
+	Apples := []Apple{}
+	var apple_loc int32
+
+	gameOver := false
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+
+		rl.DrawTexture(texture, x_coords, y_coords, rl.White)
+		rl.DrawText("Current Score: "+strconv.Itoa(score), 10, 10, 30, rl.LightGray)
+
+		if !gameOver {
+			if rl.IsKeyDown(rl.KeySpace) {
+				texture = rl.LoadTextureFromImage(bird_up)
+				y_coords -= 5
+			} else {
+				texture = rl.LoadTextureFromImage(bird_down)
+				y_coords += 5
+			}
+
+			if len(Apples) == 0 {
+				apple_loc = int32(rand.Intn(int(screenHeight - 25)))
+				current_appleImage := rl.LoadTextureFromImage(appleImage)
+				current_apple := Apple{screenWidth, apple_loc, current_appleImage}
+				Apples = append(Apples, current_apple)
+			}
+
+			for io, current_apple := range Apples {
+				rl.DrawTexture(current_apple.image, current_apple.posX, current_apple.posY, rl.White)
+				Apples[io].posX = Apples[io].posX - 5
+
+				if current_apple.posX < 0 {
+					Apples = append(Apples[:io], Apples[io+1:]...)
+					score--
+				}
+
+				if rl.CheckCollisionRecs(rl.NewRectangle(float32(x_coords), float32(y_coords), 34, 24), rl.NewRectangle(float32(current_apple.posX), float32(current_apple.posY), float32(current_apple.image.Width), float32(current_apple.image.Height)) ) {
+					Apples = append(Apples[:io], Apples[io+1:]...)
+					score++
+					rl.PlaySound(eat_noise)
+				}
+			}
+
+			if y_coords > screenHeight {
+				gameOver = true
+			}
+		} else {
+			rl.DrawText("Your final score is: "+strconv.Itoa(score), 30, 40, 30, rl.Red)
+			rl.DrawText("Press 'R' to restart", 150, 100, 20, rl.Black)
+
+			if rl.IsKeyPressed(rl.KeyR) {
+				gameOver = false
+				x_coords = screenWidth / 2
+				y_coords = screenHeight/2 - texture.Height/2 - 40
+				score = 0
+				Apples = nil
+			}
+		}
+
+		rl.EndDrawing()
+		time.Sleep(16 * time.Millisecond)
+	}
+
+	rl.StopSound(eat_noise)
+	rl.CloseWindow()
+}
